81a5583a6be60d8eff10bee4e3b82025b93037e2
Dominik <d.jagodzky@gmail.com>
2019-10-06 22:49:27 +0200

    Added movie's title in alt atribute

--- src/components/FetchPostersV2.js
+++ src/components/FetchPostersV2.js
@@ -5,17 +5,15 @@ export class FetchPosters extends React.Component {
         loading: true,
         postersPaths: null,
     };
-
+//Dodałem tytuł do src w <img/>
     async componentDidMount(){
         const url = "https://api.themoviedb.org/3/discover/movie?api_key=5ea6f181109d6b39c0b6e01983ac6ee0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=4";
         const response = await fetch(url);
         const data = await response.json();
         const arrayOfMovies = data.results;
-        const arrayOfPosters = arrayOfMovies.map((movie)=> movie.poster_path);
-        const returnPosters = arrayOfPosters.map((url)=>{
-            return <img src={`https://image.tmdb.org/t/p/w300${url}`} alt="poster"/>
-        })
-        console.log(arrayOfMovies);
+        const returnPosters = arrayOfMovies.map((movie)=>{
+            return <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title}/>
+        } );
         this.setState({postersPaths:returnPosters,loading: false})
     }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {FetchPosters} from "./components/FetchPosters";
+import {FetchPosters} from "./components/FetchPostersV2";
 
 const App = () => {
   return <FetchPosters/>;
 };
-
 ReactDOM.render(<App />, document.querySelector("#root"));
